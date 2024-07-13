c3e409276f693874d3a7d35647097868f6a41c0e
AleksTanev <tanev.aleksandar@gmail.com>
2024-07-13 08:47:33 +0300

    deleted vite assests. added reactstrap. added example component

---
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -0,0 +1,9 @@
+export interface ComponentExampleProps {
+    examples: string[];
+}
+
+const ComponentExample = ({ examples }: ComponentExampleProps) => {
+    return <></>;
+};
+
+export default ComponentExample;

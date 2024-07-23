e8961d41430499b69f6847d4b676eff25dc24b83
AleksTanev <tanev.aleksandar@gmail.com>
2024-07-24 00:16:45 +0300

    feat/added navbar and initial styling

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,26 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ComponentExample from "../shared/components/ComponentExample/ComponentExample";
+import Header from "../shared/components/Header/Header";
 
 function App() {
-  return (
-      <>
-          <BrowserRouter>
-              <Routes>
-                  <Route path="/component-example" element={<ComponentExample exampleText="test" />} />
-              </Routes>
-          </BrowserRouter>
-          <h1>Morava</h1>
-      </>
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route
+                        path="/component-example"
+                        element={
+                            <>
+                                <Header />
+                                <ComponentExample exampleText="test" />
+                            </>
+                        }
+                    />
+                </Routes>
+            </BrowserRouter>
+            <h1>Morava</h1>
+        </>
+    );
 }
 
 export default App;

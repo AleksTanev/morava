27a73039aa8a63bd5d6e83ba2dea4fd6652a12ac
nedyalkopmihalev <nedialko81@abv.bg>
2024-08-09 18:22:57 +0300

    Create Accordion Component

--- src/shared/Pages/Faq/Faq.tsx
+++ src/shared/Pages/Faq/Faq.tsx
@@ -1,13 +1,6 @@
-import {
-  Container,
-  Row,
-  Col,
-  UncontrolledAccordion,
-  AccordionItem,
-  AccordionHeader,
-  AccordionBody,
-} from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
 import PageTitle from "../../components/PageTitle/PageTitle";
+import Accordion from "../../components/Accordion/Accordion";
 import "./Faq.css";
 
 export interface FaqProps {
@@ -30,44 +23,7 @@ const Faq = () => {
               xs="12"
               className="accordion-menu"
             >
-              <UncontrolledAccordion defaultOpen={["1"]} stayOpen>
-                <AccordionItem>
-                  <AccordionHeader targetId="1">
-                    Accordion Item 1
-                  </AccordionHeader>
-                  <AccordionBody accordionId="1">
-                    This is the first item's accordion body. You can modify any
-                    of this with custom CSS or overriding our default variables.
-                    It's also worth noting that just about any HTML can go
-                    within the .accordion-body, though the transition does limit
-                    overflow.
-                  </AccordionBody>
-                </AccordionItem>
-                <AccordionItem>
-                  <AccordionHeader targetId="2">
-                    Accordion Item 2
-                  </AccordionHeader>
-                  <AccordionBody accordionId="2">
-                    This is the second item's accordion body. You can modify any
-                    of this with custom CSS or overriding our default variables.
-                    It's also worth noting that just about any HTML can go
-                    within the .accordion-body, though the transition does limit
-                    overflow.
-                  </AccordionBody>
-                </AccordionItem>
-                <AccordionItem className="accordion-item-border-bottom">
-                  <AccordionHeader targetId="3">
-                    Accordion Item 3
-                  </AccordionHeader>
-                  <AccordionBody accordionId="3">
-                    This is the third item's accordion body. You can modify any
-                    of this with custom CSS or overriding our default variables.
-                    It's also worth noting that just about any HTML can go
-                    within the .accordion-body, though the transition does limit
-                    overflow.
-                  </AccordionBody>
-                </AccordionItem>
-              </UncontrolledAccordion>
+              <Accordion />
             </Col>
             <Col
               xxl="3"

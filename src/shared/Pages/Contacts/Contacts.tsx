75540c76008d5762a6c7532272b4ef23ca560850
nedyalkopmihalev <nedialko81@abv.bg>
2024-08-09 20:33:50 +0300

    Create ContactForm component

--- src/shared/Pages/Contacts/Contacts.tsx
+++ src/shared/Pages/Contacts/Contacts.tsx
@@ -3,15 +3,11 @@ import emailjs from "@emailjs/browser";
 import {
   Container,
   Row,
-  Col,
-  Form,
-  FormGroup,
-  Label,
-  Input,
-  Button,
+  Col
 } from "reactstrap";
 import PageTitle from "../../components/PageTitle/PageTitle";
 import WorkingHours from "../../components/WorkingHours/WorkingHours";
+import ContactForm from "../../components/ContactForm/ContactForm";
 import contacts from "../../components/ContactForm/constants";
 import "./Contacts.css";
 
@@ -69,14 +65,21 @@ const Contacts = ({ contactsText }: ContactsProps) => {
       state.formName.trim().length > 2 &&
       isEmailValid(state.formEmail.trim())
     ) {
-      emailjs.sendForm(contacts.serviceID, contacts.templateID, form.current, contacts.publicKey).then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
+      emailjs
+        .sendForm(
+          contacts.serviceID,
+          contacts.templateID,
+          form.current,
+          contacts.publicKey
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
     }
   };
 
@@ -106,98 +109,16 @@ const Contacts = ({ contactsText }: ContactsProps) => {
               xs="12"
               className="right-sidebar"
             >
-              {nameFormError && (
-                <Row>
-                  <Col
-                    xxl="12"
-                    xl="12"
-                    lg="12"
-                    md="12"
-                    sm="12"
-                    xs="12"
-                    className="error-message"
-                  >
-                    {nameFormError}
-                  </Col>
-                </Row>
-              )}
-
-              {emailFormError && (
-                <Row>
-                  <Col
-                    xxl="12"
-                    xl="12"
-                    lg="12"
-                    md="12"
-                    sm="12"
-                    xs="12"
-                    className="error-message"
-                  >
-                    {emailFormError}
-                  </Col>
-                </Row>
-              )}
-              <Form onSubmit={handleFormSubmit} innerRef={form}>
-                <Input id="toName" name="toName" value={contacts.toName} type="hidden" />
-                <Row>
-                  <Col xxl="6" xl="6" lg="6" md="6" sm="12" xs="12">
-                    <FormGroup>
-                      <Label for="exampleEmail">Full Name</Label>
-                      <Input
-                        id="formName"
-                        name="formName"
-                        value={state.formName}
-                        placeholder="Full Name"
-                        type="text"
-                        className="input-form"
-                        onChange={handleInputChange}
-                      />
-                    </FormGroup>
-                  </Col>
-                  <Col xxl="6" xl="6" lg="6" md="6" sm="12" xs="12">
-                    <FormGroup>
-                      <Label for="formEmail">Email</Label>
-                      <Input
-                        id="formEmail"
-                        name="formEmail"
-                        value={state.formEmail}
-                        placeholder="Email"
-                        type="email"
-                        className="input-form"
-                        onChange={handleInputChange}
-                      />
-                    </FormGroup>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col xxl="12" xl="12" lg="12" md="12" sm="12" xs="12">
-                    <FormGroup>
-                      <Label for="formMessage">Your Message</Label>
-                      <Input
-                        id="formMessage"
-                        name="formMessage"
-                        value={state.formMessage}
-                        placeholder="Your Message"
-                        type="textarea"
-                        rows="8"
-                        className="input-form"
-                        onChange={handleInputChange}
-                      />
-                    </FormGroup>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col xxl="12" xl="12" lg="12" md="12" sm="12" xs="12">
-                    <Button
-                      className="button-display button-color"
-                      color="success"
-                      onClick={handleFormSubmit}
-                    >
-                      Send
-                    </Button>
-                  </Col>
-                </Row>
-              </Form>
+              <ContactForm
+                formRef={form}
+                nameFormError={nameFormError}
+                emailFormError={emailFormError}
+                handleFormSubmit={handleFormSubmit}
+                handleInputChange={handleInputChange}
+                stateFormName={state.formName}
+                stateFormEmail={state.formEmail}
+                stateFormMessage={state.formMessage}
+              />
             </Col>
           </Row>
         </Container>

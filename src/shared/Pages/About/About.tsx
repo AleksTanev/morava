2ba3568bf6f917ec3fa701f5694c9dafabb54c1d
nedyalkopmihalev <nedialko81@abv.bg>
2025-01-25 20:18:11 +0200

    Add styles in About and Sevices pages

--- src/shared/Pages/About/About.tsx
+++ src/shared/Pages/About/About.tsx
@@ -1,15 +1,33 @@
 import PageTitle from "../../components/PageTitle/PageTitle";
+import { Container, Row, Col } from "reactstrap";
+import styles from "./About.module.scss";
 
-export interface AboutProps {
-  aboutText: string;
-}
-
-const About = ({ aboutText }: AboutProps) => {
+const About = () => {
   return (
-    <>
+    <div className={styles["about-page"]}>
       <PageTitle pageTitleText="About" />
-      {aboutText}
-    </>
+      <Container>
+        <Row>
+          <Col xxl="6" xl="6" lg="6" md="6" sm="12" xs="12" className={styles["col-padding"]}>
+            <span className="about-text">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </span>
+          </Col>
+          <Col xxl="6" xl="6" lg="6" md="6" sm="12" xs="12">
+            Picture
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
 

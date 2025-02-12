25fe0f5e1e48e5fbd3a7b5bc7c3a63feddcfbd3a
AleksTanev <tanev.aleksandar@gmail.com>
2025-02-12 12:35:42 +0200

    fix: responsive textblock sections

--- src/shared/components/TextBlockSection/TextBlockSection.tsx
+++ src/shared/components/TextBlockSection/TextBlockSection.tsx
@@ -19,21 +19,21 @@ interface TexBlockItem {
 
 const TextBlockSection = ({ textBlockItems }: TextBlockSectionProps) => {
     const { theme } = useContext(ThemeContext);
-    return (
-        textBlockItems.length ? 
+    return textBlockItems.length ? (
         <Row className={styles["text-block-section"]}>
-            {textBlockItems.map(({icon, heading, text }, index) => (
-            <Col key={index} xl="4" xs="4" xxl="4" md="4" lg="4" sm="4" className={styles["text-block-container"]}>
-                <div className={styles["text-block"]}>
-                    <div className={styles["text-header"]}>
-                        <img src={theme === "light" ? icon.urlBlack : icon.url} alt={icon.alt} />
-                        <h3 className={styles["block-text-heading"]}>{heading}</h3>
+            {textBlockItems.map(({ icon, heading, text }, index) => (
+                <Col key={index} xl="4" xs="12" xxl="4" md="4" lg="4" sm="4" className={styles["text-block-container"]}>
+                    <div className={styles["text-block"]}>
+                        <div className={styles["text-header"]}>
+                            <img src={theme === "light" ? icon.urlBlack : icon.url} alt={icon.alt} />
+                            <h3 className={styles["block-text-heading"]}>{heading}</h3>
+                        </div>
+                        <p>{text}</p>
                     </div>
-                    <p>{text}</p>
-                </div>
-            </Col>
+                </Col>
             ))}
-        </Row>  :
+        </Row>
+    ) : (
         <></>
     );
 };
